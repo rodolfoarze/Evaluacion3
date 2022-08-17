c7330bd651fabf4531a9d0abfba4d99867920c2b
FitoGIT <arce.ag@gmail.com>
2022-08-16 23:52:10 -0400

    Agregada funcion obtener tenants

--- main.py
+++ main.py
@@ -25,27 +25,19 @@ def obtenertoken(user,password):
 
 token = obtenertoken("admin", "!v3G@!4@Y")
 
-print (token)
-
-# consultar inventario
-#getDevicesHeader = {"content-type": "application/json", "X-Auth-Token": serviceTicket}
-#getDevices = requests.get(url_controller+"/api/v1/network-device", headers=getDevicesHeader)
-
-#print(getDevices.json())
 
 
-# consultar hosts
-#getHostsHeader = {"content-type": "application/json", "X-Auth-Token": serviceTicket}
-#getHosts = requests.get(url_controller+"/api/v1/host?limit=&offset=&sortBy=&order=&hostName=&hostMac="
-                                       #"&hostType=&connectedInterfaceName=&hostIp="
-                                       #"&connectedNetworkDeviceIpAddress=&subType=&filterOperation=", headers=getHostsHeader)
+def obtenertenants():
+    url_Tenants = "https://sandboxapicdc.cisco.com/api/class/fvTenant.json"
+    requests.packages.urllib3.disable_warnings()
+    tenantsHeader = {"content-type": "application/json", "Cookie" : f"APIC-Cookie={token}"}
+    responseTenants = requests.get(url_Tenants, headers=tenantsHeader, verify=False)
+    return responseTenants
 
-#print(getHosts.json())
 
+response = obtenertenants().json()
+tenants = response['imdata']
 
-# Consultar estado de la red
+for tenant in tenants:
+    print(f"Tenant name: {tenant['fvTenant']['attributes']['name']}")
 
-#getNetworkHealthHeader = {"content-type": "application/json", "X-Auth-Token": serviceTicket}
-#getNetworkHealth = requests.get(url_controller+"/api/v1/assurance/health", headers=getNetworkHealthHeader)
-
-#print(getNetworkHealth.json())
